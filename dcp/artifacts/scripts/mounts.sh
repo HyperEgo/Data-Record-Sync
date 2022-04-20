508e96ccea23b7a8a23658a98cd152be1f19845b
Magical Zetzer <fcollins22@inbox.com>
2022-04-19 21:41:25 -0500

    update mount scripts

--- dcp/artifacts/scripts/mounts.sh
+++ dcp/artifacts/scripts/mounts.sh
@@ -6,20 +6,21 @@ MNT1='/data_local/dcp/data/dd1'
 MNT2='/data_local/dcp/data/dd2'
 DEV1='/dev/sdb'
 DEV2='/dev/sdc'
+LABEL1='DCP_DATA_01'
+LABEL2='DCP_DATA_02'
 
 # create directories, format data drives
 mkdir -p $MNT1 $MNT2
-mkfs.ext4 -F $DEV1
-mkfs.ext4 -F $DEV2
+mkfs.ext4 -F $DEV1 $DEV2
 
 # backup /etc/fstab, do not overwrite if exists
 if [ ! -f /etc/fstab.bak ]; then
-	cp -ar /etc/fstab /etc/fstab.bak
+        cp -ar /etc/fstab /etc/fstab.bak
 fi
 
-# label data drives, append to /etc/fstab, mount data drives
-e2label $DEV1 DCP_DATA_01
-e2label $DEV2 DCP_DATA_02
-echo -e "LABEL=DCP_DATA_01  $MNT1  ext4  defaults,nofail  1 2" >> /etc/fstab
-echo -e "LABEL=DCP_DATA_02  $MNT2  ext4  defaults,nofail  1 2" >> /etc/fstab
+# label data drives, append to /etc/fstab, mount
+e2label $DEV1 $LABEL1
+e2label $DEV2 $LABEL2
+echo -e "LABEL=$LABEL1  $MNT1  ext4  defaults,nofail  1 2" >> /etc/fstab
+echo -e "LABEL=$LABEL2  $MNT2  ext4  defaults,nofail  1 2" >> /etc/fstab
 mount -a
--- dcp/artifacts/scripts/remount.sh
+++ dcp/artifacts/scripts/remount.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # re-mount data drives
-
 MNT1='/data_local/dcp/data/dd1'
 MNT2='/data_local/dcp/data/dd2'
 DEV1='/dev/sdb'
 DEV2='/dev/sdc'
+LABEL1='DCP_DATA_01'
+LABEL2='DCP_DATA_02'
 
 # create directories, format data drives
 mkdir -p $MNT1 $MNT2
-mkfs.ext4 -F $DEV1 
-mkfs.ext4 -F $DEV2
+mkfs.ext4 -F $DEV1 $DEV2
 
 # re-label data drives, mount via label in /etc/fstab
-e2label $DEV1 DCP_DATA_01
-e2label $DEV2 DCP_DATA_02
+e2label $DEV1 $LABEL1
+e2label $DEV2 $LABEL2
 mount -a
